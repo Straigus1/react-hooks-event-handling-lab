1bdd528a789bd2c34f25e59f6b269fee9b5efc34
Dayton <kirylmitch@gmail.com>
2021-11-27 16:04:03 -0500

    Done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+function Keypad() {
+    function keypad(event) {
+        console.log('Entering password...')
+    }
+    return (
+        <div>
+            <input
+                type='password'
+                name='keypad'
+                placeholder='Enter password...'
+                onChange={keypad}
+            />
+
+        </div>
+    )
+}
+
+export default Keypad
